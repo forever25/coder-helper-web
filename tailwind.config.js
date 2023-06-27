1a1982493002f87085d8aaed62f2f260e787dd45
forever25 <forever__yearn@163.com>
2023-06-27 18:16:40 +0800

    fix: 修复打包错误

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   content: ['./renderer/**/*.{vue,js,ts,jsx,tsx}', './pages/**/*.{vue,js,ts,jsx,tsx}','./layout/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
