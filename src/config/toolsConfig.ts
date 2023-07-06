8b57c6754129a9ea0e9e15189ef748be845c0c29
forever25 <forever__yearn@163.com>
2023-07-06 18:13:09 +0800

    feat(add toolsconfig): 添加工具分类

--- src/config/toolsConfig.ts
+++ src/config/toolsConfig.ts
@@ -1,9 +1,22 @@
-export type ToolType = 'link' | 'inside' | ''
+export type ToolType = 'link' | 'inside'
+export type ToolCategoryKey = '数据转换' | '文本工具' | '图片工具' | '文件工具' | '视频工具' | '其他'
 
 export interface ToolInterface {
   label: string
+  ToolType: ToolType
   title: string
   detail: string
   url: string
   keyWords: string
 }
+
+export type ToolCategoryMap = {
+  [key in ToolCategoryKey]: ToolInterface[]
+}
+
+export const TOOL_MAP: Readonly<Partial<ToolCategoryMap>> = {
+  数据转换: [],
+  文本工具: []
+}
+
+export const TOOL_CATEGORY: Readonly<ToolCategoryKey[]> = ['数据转换', '文本工具', '图片工具', '视频工具', '文件工具', '其他']
